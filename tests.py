98115732a632f4062c7d5e033501f51400756ca9
cjchavez365 <chavezco@oregonstate.edu>
2026-04-27 18:36:25 -0700

    first tests

---
+++ tests.py
@@ -0,0 +1,30 @@
+import unittest
+from contrived_func import contrived_func
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        contrived_func(0)
+
+    def test2(self):
+        contrived_func(1)
+
+    def test3(self):
+        contrived_func(2)
+
+    def test4(self):
+        contrived_func(3)
+
+    def test5(self):
+        contrived_func(14)
+
+    def test6(self):
+        contrived_func(15)
+
+    def test7(self):
+        contrived_func(38)
+
+    def test8(self):
+        contrived_func(39)
+
+if __name__ = '__main__':
+    unittest.main()
